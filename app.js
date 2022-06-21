88d597d8a168e64d76a8ffa45007529b50ac3fa1
_Keyur Patel_ <_[keyur22p@gmail.com]_>
2022-06-21 11:42:16 -0500

    promises

--- app.js
+++ app.js
@@ -1,4 +1,47 @@
-console.log("Hello World!\n==========\n");
+// console.log("Hello World!\n==========\n");
+
+
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+
+
+function watchTurorialCallback(callback, errorCallback) {
+    let userLeft = false;
+  
+    if (userLeft) {
+      errorCallback("User left.");
+    } else {
+      callback("Thumbs up and Subscribe!");
+    }
+  }
+  
+  watchTurorialCallback(
+    (message) => {
+      console.log(message);
+    },
+    (error) => {
+      console.log(error.name + " " + error.message);
+    }
+  );
+
+  let promise = new Promise ((resolve, reject) => {
+    let userLeft = API.getUser();
+    if (userLeft) {
+        reject("User left.");
+      } else {
+        resolve("Thumbs up and Subscribe!");
+      }
+    });
+
+    promise.then((result) => {
+        console.log(result.msg);
+    })
+    
+    .catch((err) => {
+        console.log(err);
+    });
+
+    
+
+
+//console.log("EXERCISE 1:\n==========\n");
